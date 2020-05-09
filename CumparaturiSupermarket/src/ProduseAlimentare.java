b7d5604a079256adc5dd623bf637638428914add
Serghiev Cristian Codrut <codrut.cristian19@gmail.com>
2020-05-09 14:49:50 +0300

    Exercitiul 9

---
+++ CumparaturiSupermarket/src/ProduseAlimentare.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class ProduseAlimentare extends Produs{
+    private String alergeni;
+    private Date dataExpirare;
+
+    public ProduseAlimentare(){
+
+    }
+    public ProduseAlimentare(String nume, int pret, String alergeni, Date dataExpirare){
+        this.nume=nume;
+        this.pret=pret;
+        this.alergeni=alergeni;
+        this.dataExpirare=dataExpirare;
+    }
+    public String getAlergeni(){
+        return alergeni;
+    }
+    public Date getDataExpirare(){
+        return dataExpirare;
+    }
+
+}
